0ba8938199a5f847361174def296256d648cc33f
Matthew Goldey <matthew.goldey@gmail.com>
2022-12-02 17:08:52 -0500

    increase number of eigenvectors for which to solve from 6 to 20 to assure precision

--- speechbrain/processing/diarization.py
+++ speechbrain/processing/diarization.py
@@ -533,7 +533,10 @@ def get_oracle_num_spkrs(rec_id, spkr_info):
 
 
 def spectral_embedding_sb(
-    adjacency, n_components=8, norm_laplacian=True, drop_first=True,
+    adjacency,
+    n_components=8,
+    norm_laplacian=True,
+    drop_first=True,
 ):
     """Returns spectral embeddings.
 
@@ -602,7 +605,10 @@ def spectral_embedding_sb(
     laplacian *= -1
 
     vals, diffusion_map = eigsh(
-        laplacian, k=n_components, sigma=1.0, which="LM",
+        laplacian,
+        k=n_components,
+        sigma=1.0,
+        which="LM",
     )
 
     embedding = diffusion_map.T[n_components::-1]
@@ -618,7 +624,11 @@ def spectral_embedding_sb(
 
 
 def spectral_clustering_sb(
-    affinity, n_clusters=8, n_components=None, random_state=None, n_init=10,
+    affinity,
+    n_clusters=8,
+    n_components=None,
+    random_state=None,
+    n_init=10,
 ):
     """Performs spectral clustering.
 
@@ -662,7 +672,9 @@ def spectral_clustering_sb(
     n_components = n_clusters if n_components is None else n_components
 
     maps = spectral_embedding_sb(
-        affinity, n_components=n_components, drop_first=False,
+        affinity,
+        n_components=n_components,
+        drop_first=False,
     )
 
     _, labels, _ = k_means(
@@ -693,13 +705,16 @@ class Spec_Cluster(SpectralClustering):
 
         # Computation of affinity matrix
         connectivity = kneighbors_graph(
-            X, n_neighbors=n_neighbors, include_self=True,
+            X,
+            n_neighbors=n_neighbors,
+            include_self=True,
         )
         self.affinity_matrix_ = 0.5 * (connectivity + connectivity.T)
 
         # Perform spectral clustering on affinity matrix
         self.labels_ = spectral_clustering_sb(
-            self.affinity_matrix_, n_clusters=self.n_clusters,
+            self.affinity_matrix_,
+            n_clusters=self.n_clusters,
         )
         return self
 
@@ -884,7 +899,7 @@ class Spec_Clust_unorm:
         L = D - M
         return L
 
-    def get_spec_embs(self, L, k_oracle=4):
+    def get_spec_embs(self, L, k_oracle=4, max_eigenvectors=20):
         """Returns spectral embeddings and estimates the number of speakers
         using maximum Eigen gap.
 
@@ -905,7 +920,7 @@ class Spec_Clust_unorm:
             number of speakers then returns k_oracle.
         """
 
-        lambdas, eig_vecs = scipy.sparse.linalg.eigsh(L)
+        lambdas, eig_vecs = scipy.sparse.linalg.eigsh(L, k=max_eigenvectors)
 
         # if params["oracle_n_spkrs"] is True:
         if k_oracle is not None:
@@ -1153,7 +1168,9 @@ def do_AHC(diary_obj, out_rttm_file, rec_id, k_oracle=4, p_val=0.3):
         num_of_spk = k_oracle
 
         clustering = AgglomerativeClustering(
-            n_clusters=num_of_spk, affinity="cosine", linkage="ward",
+            n_clusters=num_of_spk,
+            affinity="cosine",
+            linkage="ward",
         ).fit(diary_obj.stat1)
         labels = clustering.labels_
 
